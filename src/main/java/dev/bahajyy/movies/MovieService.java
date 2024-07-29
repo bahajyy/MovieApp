a41ced356f7cba68ba6f4096fa08fc10d2023423
Baha Jiyan Yılmaz <baha649@gmail.com>
2024-07-30 00:52:49 +0300

    movie and review services added

--- src/main/java/dev/bahajyy/movies/MovieService.java
+++ src/main/java/dev/bahajyy/movies/MovieService.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
+
     @Autowired
-    private  MovieRepository movieRepository;
+    private MovieRepository repository;
 
-    public List<Movie> allMovies(){
-        return movieRepository.findAll();
-
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
     }
 }
